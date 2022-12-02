912db03fa12558bc5d696c97410cc01d29b3e5a2
abhinavmukwane <abhinavmukwane@gmail.com>
2022-12-02 15:08:07 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import {Helmet} from "react-helmet";
+import Home from './components/home/Home';
+import Header from "./components/header/Header";
+import './assets/css/style.css';
+import './assets/css/responsive.css';
+import './assets/css/plugins/animate.css';
+import './assets/css/plugins/swiper-bundle.min.css';
+import './assets/css/plugins/bootstrap.css';
+import './assets/css/plugins/jquery.fancybox.min.css';
+import About from "./components/about/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <body>
+    <div id="overlay_shine"></div>
+    <Header />
+    <div className="header-overlay"></div>
+    <Home />
+    <About />
+    <div class="ms-cursor"></div>
+    <div class="ms-cursor-2"></div>
+    <Helmet>
+    <script src="assets/js/plugins/jquery-3.5.1.min.js"></script>
+    <script src="assets/js/plugins/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/plugins/jquery.fancybox.min.js"></script>
+    <script src="assets/js/plugins/mixitup.min.js"></script>
+    <script src="assets/js/plugins/fontawesome.js"></script>
+    <script src="assets/js/plugins/tilt.jquery.js"></script>
+    
+    <script src="assets/js/plugins/particles.min.js"></script>
+    <script src="assets/js/plugins/app.js"></script>
+    
+    <script src="assets/js/main.js"></script>
+    </Helmet>
+  </body>
+  )
 }
 
 export default App;
---
+++ src/components/about/About.jsx
@@ -0,0 +1,163 @@
+import React from "react";
+
+const About = () => {
+
+    return (
+
+        <section className="ms-about-section ms-slide padding-tb-80 body-bg">
+        <div className="container">
+            <div className="row">
+                <div className="section-title">
+                    <h2>About<span> Me</span></h2>
+                    <span className="ligh-title">About</span>
+                </div>
+                <div className="col-lg-6">
+                    <div className="ms-about-detail">
+                        <h4>Creativity bleeds from the pen of inspiration.</h4>
+                        <p className="ms-text">I am your Brand Consultant having <b>8+ years</b> of experience in this field provides complete range of marketing materials and branding solution to any industry as well as corporate clients maintaining their reputation and increasing the brand awareness using PR & other print media & online marketing activities.</p>
+                        <div className="ms-about-info">
+                            <ul className="m-r-30">
+                                <li><span className="title">Full Name<b>:</b></span>Maria Martin Ilvor</li>
+                                <li><span className="title">Age<b>:</b></span>30 Years</li>
+                                <li><span className="title">Language<b>:</b></span>English, Hindi, Gujarati</li>
+                                <li><span className="title">Phone No<b>:</b></span>+00 9876543210</li>
+                                <li><span className="title">Email<b>:</b></span>mail.example@gmail.com</li>
+                                <li><span className="title">Address<b>:</b></span><span>Ruami Mello Moraes, - Salvador - MA, 40352, Brazil.</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-6">
+                    <div className="ms-skill-progress">
+                        <h5>PHP</h5>
+                        <div className="progress" data-percent="90%">
+                            <div className="progress-done progress-done-php" role="progressbar">
+                                <span>90%</span>
+                            </div>
+                        </div>
+                        <h5>JAVASCRIPT</h5>
+                        <div className="progress" data-percent="50%">
+                            <div className="progress-done progress-done-js" role="progressbar">
+                                <span>50%</span>
+                            </div>
+                        </div>
+                        <h5>HTML</h5>
+                        <div className="progress" data-percent="98%">
+                            <div className="progress-done progress-done-html" role="progressbar">
+                                <span>98%</span>
+                            </div>
+                        </div>
+                        <h5>CSS</h5>
+                        <div className="progress" data-percent="92%">
+                            <div className="progress-done progress-done-css" role="progressbar">
+                                <span>92%</span>
+                            </div>
+                        </div>
+                        <h5>SCSS</h5>
+                        <div className="progress" data-percent="70%">
+                            <div className="progress-done progress-done-scss" role="progressbar">
+                                <span>70%</span>
+                            </div>
+                        </div>
+                        <h5>Photoshop</h5>
+                        <div className="progress" data-percent="65%">
+                            <div className="progress-done progress-done-ps" role="progressbar">
+                                <span>65%</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row service-box p-t-80 m-tb-minus-12">
+                <div className="col-lg-3 col-md-6 col-xs-12">
+                    <div className="flipper">
+                        <div className="main-box">
+                            <div className="box-front height-300 white-bg">
+                                <img className="bg-img svg_img" src="assets/img/service/1.svg" alt="Graphics Design"></img>
+                                <div className="content-wrap">
+                                    <img className="icofont icofont-headphone-alt font-40px dark-color svg_img" src="assets/img/service/1.svg" alt="Graphics Design"></img>
+                                    <h3>Graphics Design</h3>
+                                    <p>Develop the Visual Identity of Your Business</p>
+                                </div>
+                            </div>
+                            <div className="box-back height-300 gradient-bg">
+                                <div className="content-wrap">
+                                <h3>Graphics Design</h3>
+                                    <p>Develop the Visual Identity of Your Business</p>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 col-xs-12">
+                    <div className="flipper">
+                        <div className="main-box">
+                            <div className="box-front height-300 white-bg">
+                                <img className="bg-img svg_img" src="assets/img/service/2.svg" alt="Graphics Design"></img>
+                                <div className="content-wrap">
+                                    <img className="icofont icofont-headphone-alt font-40px dark-color svg_img" src="assets/img/service/2.svg" alt="web design"></img>
+                                    <h3>Web Design</h3>
+                                    <p>Connect With Your Users, Not Just Your Business.</p>
+                                </div>
+                            </div>
+                            <div className="box-back height-300 gradient-bg">
+                                <div className="content-wrap">
+                                    <h3>Web Design</h3>
+                                    <p>Connect With Your Users, Not Just Your Business.</p>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 col-xs-12">
+                    <div className="flipper">
+                        <div className="main-box">
+                            <div className="box-front height-300 white-bg">
+                                <img className="bg-img svg_img" src="assets/img/service/3.svg" alt="Graphics Design"></img>
+                                <div className="content-wrap">
+                                    <img className="icofont icofont-headphone-alt font-40px dark-color svg_img" src="assets/img/service/3.svg" alt="development"></img>
+                                    <h3>Development</h3>
+                                    <p>We Develop the Visual Identity of Your Business.</p>
+                                </div>
+                            </div>
+                            <div className="box-back height-300 gradient-bg">
+                                <div className="content-wrap">
+                                    <h3>Development</h3>
+                                    <p>We Develop the Visual Identity of Your Business.</p>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 col-xs-12">
+                    <div className="flipper">
+                        <div className="main-box">
+                            <div className="box-front height-300 white-bg">
+                                <img className="bg-img svg_img" src="assets/img/service/4.svg" alt="Graphics Design"></img>
+                                <div className="content-wrap">
+                                    <img className="icofont icofont-headphone-alt font-40px dark-color svg_img" src="assets/img/service/4.svg" alt="seo friendly"></img>
+                                    <h3>Seo Friendly</h3>
+                                    <p>Taking your site at the top of Google's ranking.</p>
+                                </div>
+                            </div>
+                            <div className="box-back height-300 gradient-bg">
+                                <div className="content-wrap">
+                                    <h3>Seo Friendly</h3>
+                                    <p>Taking your site at the top of Google's ranking.</p>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+
+}
+
+export default About;
