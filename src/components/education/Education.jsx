d1c42c7314dce4bf1eed83d079c5ca8a01f5f329
abhinavmukwane <abhinavmukwane@gmail.com>
2023-04-11 11:52:54 +0530

    errors corrected

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -5,131 +5,6 @@ const Education = () => {
     return (
       <section className="ms-experience-section ms-slide padding-tb-80 body-bg">
         <div className="container">
-<<<<<<< HEAD
-          <div className="row">
-            <div className="section-title">
-              <h2>
-                My <span>Achievements</span>
-              </h2>
-              <span className="ligh-title">Achievements</span>
-=======
-            <div className="row">
-                <div className="section-title">
-                    <h2>My <span>Achievements</span></h2>
-                    <span className="ligh-title">Achievements</span>
-                </div>
-                <div className="col-lg-6 col-md-12 col-sm-12">
-                    <div className="education">
-                        <h4>Education</h4>
-                        <ul className="timeline">
-                            <li className="timeline-item">
-                                <div className="timeline-block">
-                                    <div className="timeline-info">
-                                        <span>Mar 2022 to Sep 2022</span>
-                                    </div>
-                                    <div className="timeline-marker"></div>
-                                    <div className="timeline-content">
-                                        <h5 className="timeline-title">Post Graduate Diploma in Advanced Computing<span className="sub">- First class (A)</span></h5>
-                                        <p>From Centre for Development of Advanced Computing (C-DAC), Pune</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li className="timeline-item">
-                                <div className="timeline-block">
-                                    <div className="timeline-info">
-                                        <span>Aug 2014 to Jun 2017</span>
-                                    </div>
-                                    <div className="timeline-marker"></div>
-                                    <div className="timeline-content">
-                                        <h5 className="timeline-title">Bachelor in Electrical Engineering<span className="sub">- First class</span></h5>
-                                        <p>From Abha Gaikwad-Patil College of Engineering, Nagpur</p>
-                                        <p>Under Rashtrasanth Tukdoji Maharaj Nagpur University</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li className="timeline-item">
-                                <div className="timeline-block">
-                                    <div className="timeline-info">
-                                        <span>Aug 2011 to Jun 2014</span>
-                                    </div>
-                                    <div className="timeline-marker"></div>
-                                    <div className="timeline-content">
-                                        <h5 className="timeline-title">Diploma in Electrical Engineering<span className="sub">- First class</span></h5>
-                                        <p>Dharampeth Polytechnic, Nagpur</p>
-                                        <p>Under Maharashtra State Board of Technical Education, Mumbai</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <div className="timeline-period-end"></div>
-                        </ul>
-                        
-                    </div>
-                </div>
-                <div className="col-lg-6 col-md-12 col-sm-12">
-                    <div className="experiense">
-                        <h4>Experiense</h4>
-                        <ul className="timeline">
-                            <li className="timeline-item">
-                                <div className="timeline-block">
-                                    <div className="timeline-info">
-                                        <span>Feb 2021 to Feb 2022</span>
-                                    </div>
-                                    <div className="timeline-marker"></div>
-                                    <div className="timeline-content">
-                                        <h5 className="timeline-title">Raychem RPG Pvt. Ltd., Pune<span className="sub">- Executive Engineer</span></h5>
-                                        <p align="justify"> Work with development teams and product managers to
-                                          ideate software solutions for DrishTi App for digital
-                                          monitoring of production. Design client-side and
-                                          server-side applications. Build the front-end of
-                                          applications through appealing visual design. Manage
-                                          databases and applications. Write effective APIs.
-                                          Created reports and dashboards using Oracle ERP to
-                                          analyze procurement performance, inventory levels, and
-                                          material usage, providing insights for
-                                          decision-making. Proficiency in using Oracle ERP for
-                                          procurement, inventory management, and supply chain
-                                          planning functions. Utilized Oracle ERP to plan and
-                                          forecast material requirements based on production
-                                          schedules, sales forecasts, and inventory levels.
-                                          Utilized Oracle ERP to identify and implement process
-                                          improvements in material planning, procurement, and
-                                          inventory management, resulting in increased
-                                          efficiency and cost savings.</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li className="timeline-item">
-                                <div className="timeline-block">
-                                    <div className="timeline-info">
-                                        <span>May 2018 to Jun 2019</span>
-                                    </div>
-                                    <div className="timeline-marker"></div>
-                                    <div className="timeline-content">
-                                        <h5 className="timeline-title">Raychem RPG Pvt. Ltd., Pune<span className="sub">- Graduate Engineer Trainee</span></h5>
-                                        <p align="justify">Assisted in the planning and scheduling of production
-                                          activities based on demand forecasts, capacity
-                                          constraints, and inventory levels. Upload weekly plan
-                                          in DrishTi App for digital monitoring of jobs
-                                          manufacturing as per plan. Using Oracle ERP for
-                                          production planning, scheduling, and reporting
-                                          functions, including generating work orders and
-                                          tracking production progress. Utilized Microsoft Excel
-                                          to create and maintain production reports, including
-                                          production schedules, inventory levels, and capacity
-                                          utilization. Collaborated with other departments,
-                                          including procurement, quality, and engineering, to
-                                          ensure smooth production operations and timely
-                                          resolution of issues.</p>
-                                    </div>
-                                </div>
-                            </li>
-                            
-                            <div className="timeline-period-end"></div>
-                        </ul>
-                    </div>
-                </div>
->>>>>>> 4e6226c2cfdc09725aec69c213c0a460c4f79e78
-            </div>
             <div className="col-lg-6 col-md-12 col-sm-12">
               <div className="education">
                 <h4>Education</h4>
@@ -269,7 +144,6 @@ const Education = () => {
               </div>
             </div>
           </div>
-        </div>
       </section>
     );
 }
